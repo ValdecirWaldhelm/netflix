379458d85398df21ea0c9206631ade091bd70dd0
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2020-11-18 18:35:23 -0300

    muito mais funcionalidades

--- js/owl/main.js
+++ js/owl/main.js
@@ -14,3 +14,30 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+let visibilidade = true;
+let info = true
+
+function ocultarexibir() {
+    
+    if(visibilidade){
+        document.getElementById("trailer").style.display = "none";
+        visibilidade = false;
+    }else{
+        document.getElementById("trailer").style.display = "block";
+        visibilidade = true;
+    }
+    
+}
+
+
+function exibirinfo() {
+
+    if(info){
+        document.getElementById("information-serie").style.display = "none";
+        info = false;
+    }else{
+        document.getElementById("information-serie").style.display = "block";
+        info = "true"
+    }
+}
